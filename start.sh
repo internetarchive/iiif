eadc7fcbcf07dbb160f81ac8811826411a3674ad
Robert Keizer <rob@archive.org>
2023-09-05 10:55:25 -0500

    Removing debugging line to kick github actions.

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-export
-
 envsubst '$NOMAD_HOST_ADDR_cantaloupe' < nginx-vhost.conf > /etc/nginx/conf.d/nginx.conf
 
 envsubst '$NOMAD_HOST_ADDR_cantaloupe' < settings.cfg > iiify/configs/settings.cfg
