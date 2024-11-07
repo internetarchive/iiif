edd713755de944ab330f877cae57e62dbe94b095
Glen Robson <glen.robson@gmail.com>
2024-11-07 21:31:24 +0000

    Adding new Collection helper

---
+++ tests/test_helper.py
@@ -0,0 +1,23 @@
+import os
+os.environ["FLASK_CACHE_DISABLE"] = "true"
+
+import unittest
+from flask.testing import FlaskClient
+from iiify.app import app
+
+class TestHelper(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.test_app = FlaskClient(app)
+
+    def test_single_image(self):
+        resp = self.test_app.get("/iiif/helper/img-8664_202009/")
+        self.assertEqual(resp.status_code, 200)
+
+        self.assertIn('<a href="https://projectmirador.org/embed/?iiif-content=https://iiif.archive.org/iiif/3/img-8664_202009/manifest.json">Mirador</a>', resp.text, "Couldn't find Mirador link in helper page.")
+
+    def test_collection(self):
+        resp = self.test_app.get("/iiif/helper/frankbford/")
+        self.assertEqual(resp.status_code, 200)
+
+        self.assertIn('<a href="https://projectmirador.org/embed/?iiif-content=https://iiif.archive.org/iiif/3/frankbford/collection.json">Mirador</a>', resp.text, "Couldn't find Mirador link in helper page.")
