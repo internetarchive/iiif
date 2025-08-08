9df36ed77de2ee2f8053d3cf42590efd1a737eb4
Glen Robson <glen.robson@gmail.com>
2025-08-08 15:25:27 +0100

    Checking its the correct size

--- tests/test_audio.py
+++ tests/test_audio.py
@@ -33,8 +33,8 @@ class TestAudio(unittest.TestCase):
             accCanvas = canvas['accompanyingCanvas']
             self.assertEqual(accCanvas["type"], "Canvas")
             self.assertEqual(accCanvas["label"]["en"][0], "Waveform")
-            self.assertTrue("height" in accCanvas)
-            self.assertTrue("width" in accCanvas)
+            self.assertTrue("height" in accCanvas and accCanvas["height"] == 200)
+            self.assertTrue("width" in accCanvas and accCanvas["width"] == 200)
 
     def test_multi_track_audio_gets_ranges(self):
         resp = self.test_app.get("/iiif/Weirdos_demo-1978/manifest.json")
