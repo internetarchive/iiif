cdeccac85951be374a6adce374ed892ebc06bbc2
Glen Robson <glen.robson@gmail.com>
2026-01-09 00:48:47 +0000

    Fixing issue of missing items in collection

--- iiify/resolver.py
+++ iiify/resolver.py
@@ -184,8 +184,7 @@ def create_collection3(identifier, domain, page=1, rows=MAX_API_LIMIT):
     addMetadata(collection, identifier, metadata['metadata'], collection=True)
     addPartOfCollection(collection, metadata.get('metadata').get('collection', []), domain)
 
-    asURL = f'{ADVANCED_SEARCH}?q=collection%3A{identifier}&fl[]=identifier&fl[]=mediatype&fl[]=title&fl[]=description&sort[]=&sort[]=&sort[]=&rows={rows}&page={page}&output=json&save=yes'
-    itemsSearch = retrieve_collection(identifier, rows, page)
+    itemsSearch = retrieve_collection(identifier, page, rows)
     total = itemsSearch['response']['numFound']
     # There is a max of 10,000 items that can be retrieved from the advanced search
     if total > MAX_SCRAPE_LIMIT:
